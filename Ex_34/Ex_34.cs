509d8aeb5770c0c0d237c3c36cacc445cfcc2a51
Vadim <lyzhinv@gmail.com>
2022-10-17 14:06:13 +0300

    Add Exercise 34

---
+++ Ex_34/Ex_34.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве. [345, 897, 568, 234] -> 2
+
+int[] baseArray = GetArray(4, 100, 999);
+
+Console.WriteLine($"Количество четных чисел в масиве [{String.Join(" ", baseArray)}] -> {String.Join(" ", ResultArray(baseArray))}");
+
+
+int ResultArray (int[] array) // Метод подсчета четных чисел в массиве
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int[] GetArray(int size, int minValue, int maxValue) // Метод создания случаного массива
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
